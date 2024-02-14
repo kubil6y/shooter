745796e83a107d4962fc35fb4bfc92060e7c1d6e
kubil6y <lieqb2@gmail.com>
2024-02-14 23:23:57 +0300

    add muzzle flash

--- Assets/_Scripts/Weapons/ProjectileWeaponEffects.cs
+++ Assets/_Scripts/Weapons/ProjectileWeaponEffects.cs
@@ -1,12 +1,19 @@
 using System;
+using System.Collections;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 [RequireComponent(typeof(ProjectileWeapon))]
 public class ProjectileWeaponEffects : MonoBehaviour {
+	[SerializeField] private Light2D m_muzzleFlashLight;
+
 	private ProjectileWeapon m_projectileWeapon;
 	private CinemachineImpulseSource m_impulseSource;
 	private Animator m_animator;
+
+	private float m_muzzleFlashDuration = .1f;
+	private Coroutine m_muzzleFlashRoutine;
 
 	private readonly int ANIMKEY_FIRE = Animator.StringToHash("WeaponFire");
 
@@ -20,7 +27,21 @@ public class ProjectileWeaponEffects : MonoBehaviour {
 		m_projectileWeapon.OnShoot += ProjectileWeapon_OnShoot;
 	}
 
+	private void MuzzleFlash() {
+		if (m_muzzleFlashRoutine != null) {
+			StopCoroutine(m_muzzleFlashRoutine);
+		}
+		m_muzzleFlashRoutine = StartCoroutine(MuzzleFlashRoutine());
+	}
+
+	private IEnumerator MuzzleFlashRoutine() {
+		m_muzzleFlashLight.gameObject.SetActive(true);
+		yield return new WaitForSeconds(m_muzzleFlashDuration);
+		m_muzzleFlashLight.gameObject.SetActive(false);
+	}
+
 	private void ProjectileWeapon_OnShoot(object sender, EventArgs e) {
+		MuzzleFlash();
 		m_animator.Play(ANIMKEY_FIRE, 0, 0f);
 		m_impulseSource.GenerateImpulse();
 	}

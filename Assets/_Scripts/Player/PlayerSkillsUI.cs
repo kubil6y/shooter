703ca8b0c3407a3d1017a233ff5d733b72f6cc61
kubil6y <lieqb2@gmail.com>
2024-02-26 13:53:00 +0300

    fix PlayerSkillsUI

--- Assets/_Scripts/Player/PlayerSkillsUI.cs
+++ Assets/_Scripts/Player/PlayerSkillsUI.cs
@@ -1,10 +1,32 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerSkillsUI : MonoBehaviour {
 	[SerializeField] private Image m_ultimateImage;
+	private bool m_isUltimateOnCooldown;
+
+	private void Start() {
+		Player.instance.animations.OnAnimUltimateFire += Player_OnAnimUltimateFire;
+		Player.instance.skills.OnUltimateOutOfCooldown += Player_OnUltimateOutOfCooldown;
+	}
 
 	private void Update() {
-		m_ultimateImage.fillAmount = Player.instance.skills.GetUltimateTimerNormalized();
+        UpdateUltimateCooldownVisual();
+    }
+
+    private void UpdateUltimateCooldownVisual() {
+        if (m_isUltimateOnCooldown) {
+            m_ultimateImage.fillAmount = Player.instance.skills.GetUltimateTimerNormalized();
+        }
+    }
+
+    private void Player_OnAnimUltimateFire(object sender, EventArgs e) {
+		m_isUltimateOnCooldown = true;
 	}
+
+	private void Player_OnUltimateOutOfCooldown(object sender, EventArgs e) {
+		m_isUltimateOnCooldown = false;
+	}
+
 }

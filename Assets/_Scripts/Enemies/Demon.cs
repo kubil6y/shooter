53efcbb214482d12c4ab4553cfabee35bd1d085b
kubil6y <lieqb2@gmail.com>
2024-03-03 00:25:56 +0300

    add skeleton demon

--- Assets/_Scripts/Enemies/Demon.cs
+++ Assets/_Scripts/Enemies/Demon.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 public class Demon : BaseEnemy {
 	[SerializeField] private float m_moveSpeed = 4f;
 
+	private DemonAnimations m_animations;
+
+	public enum State {
+		Chasing,
+		Attack,
+	}
+
 	private Vector2 m_moveDir;
+	private State m_state;
+
+	protected override void Awake() {
+		base.Awake();
+		m_animations = GetComponentInChildren<DemonAnimations>();
+	}
+
+	protected override void Start() {
+		base.Start();
+		m_animations.OnAttacked += Demon_OnAttacked;
+	}
 
 	private void Update() {
 		FollowPlayer();
@@ -16,5 +34,10 @@ public class Demon : BaseEnemy {
 
 	private void FollowPlayer() {
 		m_moveDir = (Player.instance.transform.position - transform.position).normalized;
+	}
+
+	private void Demon_OnAttacked(object sender, EventArgs e) {
+		// TODO spawn some things!
+		Debug.Log("Demon_OnAttacked()");
 	}
 }
---
+++ Assets/_Scripts/Enemies/DemonAnimator.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class DemonAnimations : MonoBehaviour {
+	public event EventHandler OnAttacked;
+
+	private Demon m_demon;
+	private Animator m_animator;
+
+	private readonly int ANIMKEY_CHASE = Animator.StringToHash("Chase");
+	private readonly int ANIMKEY_ATTACK = Animator.StringToHash("Attack");
+
+	private void Awake() {
+		m_animator = GetComponent<Animator>();
+		m_demon = GetComponentInParent<Demon>();
+	}
+
+	public void AnimAttackTrigger() {
+		OnAttacked?.Invoke(this, EventArgs.Empty);
+	}
+
+	#region Animation Setters
+	public void SetChaseAnim(bool value) {
+		m_animator.SetBool(ANIMKEY_CHASE, value);
+	}
+
+	public void SetAttackAnim(bool value) {
+		m_animator.SetBool(ANIMKEY_ATTACK, value);
+	}
+	#endregion // Animation Setters
+}

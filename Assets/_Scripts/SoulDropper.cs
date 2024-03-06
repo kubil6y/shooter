19c91a07a02668ef671f8dacb1a260533a384e6a
kubil6y <lieqb2@gmail.com>
2024-03-06 22:06:25 +0300

    improve soul dropper

--- Assets/_Scripts/SoulDropper.cs
+++ Assets/_Scripts/SoulDropper.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class SoulDropper : MonoBehaviour {
 	[SerializeField] private GameObject m_soulPrefab;
+	[SerializeField] private int m_soulAmount = 1;
+	[SerializeField] private float m_soulOffsetDistance = 1f;
 	private Health m_health;
 
 	private void Awake() {
@@ -13,9 +15,33 @@ public class SoulDropper : MonoBehaviour {
 		m_health.OnDeath += Health_OnDeath;
 	}
 
+	private void Update() {
+		if (Input.GetKeyDown(KeyCode.T)) {
+			SpawnPrefab();
+		}
+	}
+
 	private void SpawnPrefab() {
-		GameObject soulObject = Instantiate(m_soulPrefab);
-		soulObject.transform.position = transform.position;
+		if (m_soulAmount <= 0) {
+			Debug.LogError($"Soul amount {m_soulAmount} is stupid. You are stupid.");
+		}
+		if (m_soulAmount == 1) {
+			GameObject soulObject = Instantiate(m_soulPrefab);
+			soulObject.transform.position = transform.position;
+		}
+		else {
+			float spreadAngle = 360f / m_soulAmount;
+
+			for (int i = 0; i < m_soulAmount; i++) {
+				float angle = spreadAngle * i * Mathf.Deg2Rad;
+				float xOffset = Mathf.Cos(angle) * m_soulOffsetDistance;
+				float yOffset = Mathf.Sin(angle) * m_soulOffsetDistance;
+				Vector3 offset = new Vector3(xOffset, yOffset, 0f);
+
+				GameObject soulObject = Instantiate(m_soulPrefab);
+				soulObject.transform.position = transform.position + offset;
+			}
+		}
 	}
 
 	private void Health_OnDeath(object sender, EventArgs e) {
--- Assets/_Scripts/Testing.cs
+++ Assets/_Scripts/Testing.cs
@@ -4,17 +4,17 @@ public class Testing : MonoBehaviour {
 	public GameObject enemyObject;
 
 	private void Update() {
-		if (Input.GetKeyDown(KeyCode.L)) {
-			Player.instance.health.TakeDamage(25);
-		}
+		// if (Input.GetKeyDown(KeyCode.L)) {
+		// 	Player.instance.health.TakeDamage(25);
+		// }
 
-		if (Input.GetKeyDown(KeyCode.T)) {
-			Player.instance.transform.position = Vector2.zero;
-		}
+		// if (Input.GetKeyDown(KeyCode.T)) {
+		// 	Player.instance.transform.position = Vector2.zero;
+		// }
 
-		if (Input.GetKeyDown(KeyCode.O)) {
-			Player.instance.Revive();
-			Debug.Log("Blink duration: " + Player.instance.blink.GetBlinkDuration());
-		}
+		// if (Input.GetKeyDown(KeyCode.O)) {
+		// 	Player.instance.Revive();
+		// 	Debug.Log("Blink duration: " + Player.instance.blink.GetBlinkDuration());
+		// }
 	}
 }
